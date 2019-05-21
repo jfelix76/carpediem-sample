a9925c7e22224037db53cb707c1bc4d6f82a5da3
ngJosh <joshfelix@stantoninnovations.com>
2019-05-21 00:18:38 -0400

    added material + tabs

--- apps/angular-app/src/app/app.module.ts
+++ apps/angular-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { GreetingElement } from '@carpediem-sample/ui';
-
+import { MatButtonModule, MatCheckboxModule, MatTabsModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTabsModule
+  ],
   providers: [],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
